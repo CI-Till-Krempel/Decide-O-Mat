e6d7dc3d5414ce8627eefabb618f36c338e14aa1
Till Krempel <till.krempel@cologne-intelligence.de>
2025-12-09 10:40:23 +0100

    fix: allow process global in vite config to pass lint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -30,4 +30,12 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+  {
+    files: ['vite.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
 ])
