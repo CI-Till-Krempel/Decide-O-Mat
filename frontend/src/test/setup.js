b2677b7fad243a0e971a7e476965a4e834d19cac
Boy-Scout Coding Agent <boy-scout@decide-o-mat.local>
2026-09-21 10:22:48 +0000

    Boy-Scout: Mock HTMLCanvasElement in JSDOM tests to silence warnings

--- frontend/src/test/setup.js
+++ frontend/src/test/setup.js
@@ -42,3 +42,35 @@ Object.defineProperty(navigator, 'clipboard', {
     writable: true,
     configurable: true,
 });
+
+// Mock HTMLCanvasElement for environments without canvas npm package installed (e.g., JSDOM in Vitest/CI)
+if (typeof window !== 'undefined') {
+    HTMLCanvasElement.prototype.getContext = () => ({
+        fillRect: () => {},
+        clearRect: () => {},
+        getImageData: (x, y, w, h) => ({
+            data: new Uint8ClampedArray(w * h * 4)
+        }),
+        putImageData: () => {},
+        createImageData: () => [],
+        setTransform: () => {},
+        drawImage: () => {},
+        save: () => {},
+        restore: () => {},
+        beginPath: () => {},
+        moveTo: () => {},
+        lineTo: () => {},
+        closePath: () => {},
+        stroke: () => {},
+        translate: () => {},
+        scale: () => {},
+        rotate: () => {},
+        arc: () => {},
+        fill: () => {},
+        measureText: () => ({ width: 0 }),
+        transform: () => {},
+        rect: () => {},
+        clip: () => {},
+    });
+    HTMLCanvasElement.prototype.toDataURL = () => '';
+}
