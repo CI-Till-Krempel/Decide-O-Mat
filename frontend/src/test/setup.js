f4df93f2f7602e87044158a69fe9f42bd147dad8
Boy-Scout Agent <boy-scout@decide-o-mat.local>
2026-09-22 09:38:40 +0000

    Boy-Scout: silence JSDOM canvas warnings & update ESLint version

--- frontend/src/test/setup.js
+++ frontend/src/test/setup.js
@@ -42,3 +42,34 @@ Object.defineProperty(navigator, 'clipboard', {
     writable: true,
     configurable: true,
 });
+
+// Stub HTMLCanvasElement.prototype.getContext to prevent noisy JSDOM warnings during testing
+if (typeof HTMLCanvasElement !== 'undefined') {
+    HTMLCanvasElement.prototype.getContext = () => {
+        return {
+            fillRect: () => {},
+            clearRect: () => {},
+            getImageData: (x, y, w, h) => ({ data: new Array(w * h * 4).fill(0) }),
+            putImageData: () => {},
+            createImageData: () => [],
+            setTransform: () => {},
+            drawImage: () => {},
+            save: () => {},
+            restore: () => {},
+            beginPath: () => {},
+            moveTo: () => {},
+            lineTo: () => {},
+            closePath: () => {},
+            stroke: () => {},
+            translate: () => {},
+            scale: () => {},
+            rotate: () => {},
+            arc: () => {},
+            fill: () => {},
+            measureText: () => ({ width: 0 }),
+            transform: () => {},
+            rect: () => {},
+            clip: () => {},
+        };
+    };
+}
